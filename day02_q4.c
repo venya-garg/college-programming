f0d89be62d89e651b64f6187680ae832ae159e79
Venya Garg <venyagarg044@gmail.com>
2026-08-16 02:21:38 +0530

    Day 2 Q4 solution

---
+++ day02_q4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+float pi = 3.14;
+int r;
+printf("Enter radius of circle: ");
+scanf("%d", &r);
+
+float area = pi * r * r;
+float circumference = 2 * pi * r;
+
+printf("The area of circle is: %f\n", area);
+printf("The circumference of circle is: %f\n", circumference);
+
+return 0;
+}
+
