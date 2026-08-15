6eb6b4a289b5d884c7498fbd64c6ce467a22bc3a
Venya Garg <venyagarg044@gmail.com>
2026-08-16 01:52:18 +0530

    Rename file to Day 2 Q3

---
+++ program3.c
@@ -0,0 +1,11 @@
+//Write a program to find simple interest
+#include <stdio.h>
+int main() {
+int P = 100;
+int R = 5;
+int T = 6;
+int SI = P*R*T/100;
+printf("Simple Interest: %d \n" ,SI);
+
+return 0;
+}
