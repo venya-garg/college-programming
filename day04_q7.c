16d081c5ba8a8db790ddf2265804ff960577568a
Venya Garg <venyagarg044@gmail.com>
2026-08-16 15:25:03 +0530

    Day 4 Q7 solution

---
+++ day04_q7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int a,b;
+a = 10;
+b = 20;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+printf("New value of a is: %d\n", a);
+printf("New value of b is: %d\n", b);
+
+return 0;
+}
