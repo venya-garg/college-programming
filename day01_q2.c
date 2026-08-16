094825d0073b2f8ec6544b3a9ba4c31f51035fb1
Venya Garg <venyagarg044@gmail.com>
2026-08-16 16:00:58 +0530

    Day 1 Q2 solution

--- day01_q2.c
+++ day01_q2.c
@@ -4,8 +4,10 @@
 
 int main() {
 int a,b;
-printf("Enter two numbers: ");
-scanf("%d %d", &a, &b);
+printf("Enter value of a: ");
+scanf("%d", &a);
+printf("Enter value of b: ");
+scanf("%d", &b);
 
 int sum = a + b;
 int difference = a - b;
