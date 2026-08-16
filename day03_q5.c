7a6564c9c60d6cdb7a3031361783c29b3d725273
Venya Garg <venyagarg044@gmail.com>
2026-08-16 14:55:34 +0530

    Day 3 Q5 solution

---
+++ day03_q5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+float C;
+printf("Enter the value of temp. in Celsius: ");
+scanf("%f", &C);
+
+float F = C * 1.8 + 32;
+printf("The value of temp. in Fehrenheit is: %f\n", F);
+return 0;
+}
