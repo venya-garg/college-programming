c4484882d30c775982b0b23d89c7723f82d91e62
Venya Garg <venyagarg044@gmail.com>
2026-08-27 12:31:49 +0530

    Day 18 Q35 solution

---
+++ day18_q35.c
@@ -0,0 +1,23 @@
+//Write a program to print all factors of a given number.
+
+#include <stdio.h>
+
+int main()
+{
+int num, i;
+
+printf("Enter a number: ");
+scanf("%d", &num);
+
+printf("Factors of %d are:\n", num);
+
+for (i = 1; i <= num; i++)
+{
+if (num % i == 0)
+{
+printf("%d \n", i);
+}
+}
+
+return 0;
+}
