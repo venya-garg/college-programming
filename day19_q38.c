46fbd81269cc1046306f3451007ea1400928cab3
Venya Garg <venyagarg044@gmail.com>
2026-08-29 22:19:10 +0530

    Day 19 Q38 solution

---
+++ day19_q38.c
@@ -0,0 +1,22 @@
+//Write a program to find the sum of digits of a number.
+
+#include <stdio.h>
+
+int main()
+{
+int num, digit, sum = 0;
+
+printf("Enter a number: ");
+scanf("%d", &num);
+
+while (num != 0)
+{
+digit = num % 10;
+sum = sum + digit;
+num = num / 10;
+}
+
+printf("Sum of digits = %d\n", sum);
+
+return 0;
+} 
