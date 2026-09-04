3987f3530f5e8b22e17f84d698071bf97b3f1b4f
Venya Garg <venyagarg044@gmail.com>
2026-09-04 22:33:09 +0530

    Day 26 Q52 solution

---
+++ day26_q52.c
@@ -0,0 +1,46 @@
+/* Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+* */
+
+#include <stdio.h>
+
+int main()
+{
+int i, j;
+
+for (i = 1; i <= 5; i += 2)
+{
+for (j = 1; j <= i; j++)
+{
+printf("*\n");
+}
+printf("\n");
+}
+
+for (i = 3; i >= 1; i -= 2)
+{
+for (j = 1; j <= i; j++)
+{
+printf("*\n");
+}
+printf("\n");
+}
+
+return 0;
+}
