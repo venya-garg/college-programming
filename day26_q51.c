b6ec43d426970ca7a9035901dfeb10c9a1e635f3
Venya Garg <venyagarg044@gmail.com>
2026-09-04 22:22:05 +0530

    Day 26 Q51 solution

---
+++ day26_q51.c
@@ -0,0 +1,30 @@
+/* Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345 */
+
+#include <stdio.h>
+
+int main()
+{
+int i, j;
+
+for (i = 1; i <= 5; i++)
+{
+for (j = 1; j <= 5 - i; j++)
+{
+printf(" ");
+}
+
+for (j = 6 - i; j <= 5; j++)
+{
+printf("%d", j);
+}
+
+printf("\n");
+}
+
+return 0;
+}
