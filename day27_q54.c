4c45051d1b1eb85a2bcfff7d10ab108e32ad1ed2
Venya Garg <venyagarg044@gmail.com>
2026-09-05 14:36:06 +0530

    Day 27 Q54 solution

---
+++ day27_q54.c
@@ -0,0 +1,52 @@
+/* Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   * */
+
+#include <stdio.h>
+
+int main()
+{
+int i, j, spaces;
+
+for (i = 1; i <= 7; i += 2)
+{
+spaces = (7 - i) / 2;
+
+for (j = 1; j <= spaces; j++)
+{
+printf(" ");
+}
+
+for (j = 1; j <= i; j++)
+{
+printf("*");
+}
+
+printf("\n");
+}
+
+for (i = 5; i >= 1; i -= 2)
+{
+spaces = (7 - i) / 2;
+
+for (j = 1; j <= spaces; j++)
+{
+printf(" ");
+}
+
+for (j = 1; j <= i; j++)
+{
+printf("*");
+}
+
+printf("\n");
+}
+
+return 0;
+}
