c9e757af3ad4fdd05489d23af69cc6c79fc752fd
Venya Garg <venyagarg044@gmail.com>
2026-09-11 22:37:34 +0530

    Day 33 Q66 soltuion

---
+++ day33_q66.c
@@ -0,0 +1,50 @@
+//Insert an element in a sorted array at the appropriate position.
+
+#include<stdio.h>
+
+int main()
+{
+    int n, i, element, pos;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int a[n+1];
+
+    printf("Enter the elements in sorted order: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    pos = n;
+
+    for(i=0; i<n; i++)
+    {
+        if(element < a[i])
+        {
+            pos = i;
+            break;
+        }
+    }
+
+    for(i=n; i>pos; i--)
+    {
+        a[i] = a[i-1];
+    }
+
+    a[pos] = element;
+    n++;
+
+    printf("Array after insertion: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
