30633c4f172378f9b12dce3c52df97981d8ed92f
Venya Garg <venyagarg044@gmail.com>
2026-09-16 12:33:28 +0530

    Day 38 Q76 solution

--- day38_q76.c
+++ day38_q76.c
@@ -4,9 +4,12 @@
 
 int main() {
 int a[10][10];
-int n, i, flag = 1;
+int n, i, j, flag = 1;
 
 printf("Enter the order of square matrix: ");
+scanf("%d", &n);
+
+printf("Enter matrix elements:\n");
 
 for(i = 0; i < n; i++) {
 for(j = 0; j < n; j++) {
@@ -16,4 +19,17 @@ scanf("%d", &a[i][j]);
 
 for(i = 0; i < n; i++) {
 for(j = 0; j < n; j++) {
+if(a[i][j] != a[j][i]) {
+flag = 0;
+break;
+}
+}
+}
 
+if(flag == 1)
+printf("Matrix is symmetric.\n");
+else
+printf("Matrix is not symmetric.\n");
+
+return 0;
+}
