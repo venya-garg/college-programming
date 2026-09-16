d92ed0137893e193dc95580367d18fe45a8bf208
Venya Garg <venyagarg044@gmail.com>
2026-09-16 12:27:27 +0530

    Day 38 Q76 solution

---
+++ day38_q76.c
@@ -0,0 +1,19 @@
+//Check if a matrix is symmetric.
+
+#include <stdio.h>
+
+int main() {
+int a[10][10];
+int n, i, flag = 1;
+
+printf("Enter the order of square matrix: ");
+
+for(i = 0; i < n; i++) {
+for(j = 0; j < n; j++) {
+scanf("%d", &a[i][j]);
+}
+}
+
+for(i = 0; i < n; i++) {
+for(j = 0; j < n; j++) {
+
