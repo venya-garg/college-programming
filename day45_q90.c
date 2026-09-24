213612772e90b46c63103c125b0fccfd260b5cec
Venya Garg <venyagarg044@gmail.com>
2026-09-24 13:11:02 +0530

    Day 45 Q90 solution

---
+++ day45_q90.c
@@ -0,0 +1,23 @@
+//Toggle case of each character in a string.
+
+#include <stdio.h>
+
+int main() {
+char str[100];
+int i;
+
+printf("Enter a string: ");
+fgets(str, sizeof(str), stdin);
+
+for(i = 0; str[i] !=  '\0'; i++) {
+if(str[i] >= 'A' && str[i] <= 'Z')
+str[i] = str[i] + 32;
+
+else if(str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
+
+printf("After toggling: %s", str);
+
+return 0;
+}
